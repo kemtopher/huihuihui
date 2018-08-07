3fd1dc7cba60c0a703367445ea82d002d4f2e6a6
Christopher Kemeza <ckemeza1@gmail.com>
2018-08-07 19:42:35 -0400

    ..

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  get 'projects/new'
-  get 'projects/create'
-  get 'projects/update'
-  get 'projects/edit'
-  get 'projects/destroy'
-  get 'projects/index'
-  get 'projects/show'
+  # get 'projects/new'
+  # get 'projects/create'
+  # get 'projects/update'
+  # get 'projects/edit'
+  # get 'projects/destroy'
+  # get 'projects/index'
+  # get 'projects/show'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :projects
-  # root 'pages#index'
-  root 'projects#new'
+  root 'pages#index'
+  # root 'projects#new'
 end
