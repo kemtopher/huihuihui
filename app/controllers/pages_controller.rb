99473267a4b187a0b00c140a022e772756dce0b0
Christopher Kemeza <ckemeza1@gmail.com>
2018-08-07 22:01:16 -0400

    fixed sorting problem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,10 +10,10 @@ class PagesController < ApplicationController
   	# @special_project = Project.find(9)
   	# @bottom_projects = @projects[10..17]
 
-  	@top_projects = @sorted.limit(4)
-  	@mid_projects = @sorted.offset(4).limit(4)
-  	@special_project = @sorted[8]
-  	@bottom_projects = @sorted.offset(9).limit(8)  
+  	@top_projects = @projects.limit(4)
+  	@mid_projects = @projects.offset(4).limit(4)
+  	@special_project = @projects[8]
+  	@bottom_projects = @projects.offset(9).limit(8)  
   end
 
 
