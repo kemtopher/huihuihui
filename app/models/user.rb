26de66e2edfa3df8530fe6e2e0cf65ea35921857
Christopher Kemeza <ckemeza1@gmail.com>
2018-08-11 02:55:27 -0400

    found duplicate in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
 
   # REMOVED :registerable, so that only one user may exist. 
   # place back below if you want to open up registation abilities again
