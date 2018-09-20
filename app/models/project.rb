cce183f89b17a382d49a4878b5a9fe8ec03e44dc
Christopher Kemeza <ckemeza1@gmail.com>
2018-09-20 15:09:28 -0400

    ..

--- app/models/project.rb
+++ app/models/project.rb
@@ -12,7 +12,7 @@ class Project < ApplicationRecord
 	## PROJECTS NOT LOOPING FROM END TO BEGINNING 
 	## PROJECTS ALLOWING IN-ACTIVE PRODUCTS TO SORT
 	def next
-		Project.where("position > ?", position).order(position: :asc).first || Project.order(position: :asc).first
+		Project.where("position > ?", position).order(position: :asc).first || Project.where(:position => 1..17).order(position: :asc).first
 	end 
 
 	def prev
