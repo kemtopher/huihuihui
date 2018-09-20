2c66e1b433233befd6fc8d37a99037a578defc04
Christopher Kemeza <ckemeza1@gmail.com>
2018-09-20 15:12:57 -0400

    debugging bs

--- app/models/project.rb
+++ app/models/project.rb
@@ -12,10 +12,10 @@ class Project < ApplicationRecord
 	## PROJECTS NOT LOOPING FROM END TO BEGINNING 
 	## PROJECTS ALLOWING IN-ACTIVE PRODUCTS TO SORT
 	def next
-		Project.where("position > ?", position).order(position: :asc).first || Project.where(:position => 1..17).order(position: :asc).first
+		Project.where(:position => 1..17).where("position > ?", position).order(position: :asc).first || Project.where(:position => 1..17).order(position: :asc).first
 	end 
 
 	def prev
-		Project.where("position < ?", position).order(position: :desc).first || Project.where(:position => 1..17).order(position: :desc).first
+		Project.where(:position => 1..17).where("position < ?", position).order(position: :desc).first || Project.where(:position => 1..17).order(position: :desc).first
 	end
 end
