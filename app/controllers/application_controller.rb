e16e82015f9864a9384541dada373e4af0ece110
Christopher Kemeza <ckemeza1@gmail.com>
2020-01-08 12:42:46 -0500

    changed bio and ext nav links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 	private 
 	
 	def bio_info
-		render html: '<p>Hui Chen Ou Yang is a multidisciplinary designer based in New York. She has spent her life living in various cities: Surabaya, Indonesia; Taipei, Taiwan; San Francisco, U.S.A.; and now calls New York City her home. Hui has worked at places such as Apple, Siegel+Gale, Elephant, Small Stuff, and her work has been recognized by the Type Directors Club and Art Director’s Club Chinese Young Guns. Currently, she is going through a lot of underground feminist comix, listening to occult podcasts, and making essential oil blends.</p>'.html_safe
+		render html: '<p>Hui OY is a multidisciplinary designer based in New York. She has spent her life living in various cities: Surabaya, Indonesia; Taipei, Taiwan; San Francisco, U.S.A.; and now calls New York City her home. Hui has worked at places such as Apple, 2×4, Siegel+Gale, and her work has been recognized by the Type Directors Club and Art Director’s Club Chinese Young Guns. Currently, she is going through a lot of underground feminist comix, listening to occult podcasts, and making essential oil blends.</p>'.html_safe
 	end
 
 end
