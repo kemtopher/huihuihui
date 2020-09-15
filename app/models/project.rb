89f3c3b86b0e498da43866d26896a69073ed9482
Christopher Kemeza <ckemeza1@gmail.com>
2020-09-15 16:29:33 -0400

    Limited projects to most recent 8

--- app/models/project.rb
+++ app/models/project.rb
@@ -10,10 +10,10 @@ class Project < ApplicationRecord
 	## PROJECTS NAVIGATION LOOPS AT END AND BEGINNING 
 
 	def next
-		Project.where(:position => 1..17).where("position > ?", position).order(position: :asc).first || Project.where(:position => 1..17).order(position: :asc).first
+		Project.where(:position => 1..7).where("position > ?", position).order(position: :asc).first || Project.where(:position => 1..7).order(position: :asc).first
 	end 
 
 	def prev
-		Project.where(:position => 1..17).where("position < ?", position).order(position: :desc).first || Project.where(:position => 1..17).order(position: :desc).first
+		Project.where(:position => 1..7).where("position < ?", position).order(position: :desc).first || Project.where(:position => 1..7).order(position: :desc).first
 	end
 end
